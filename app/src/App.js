21e50dc464263a2da847e9b3abf0287d17ad0cf4
Daniel <satelerd@gmail.com>
2022-04-16 19:07:47 -0400

    page displaying the url info

--- app/src/App.js
+++ app/src/App.js
@@ -1,17 +1,19 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+// import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Info from './components/Info/index';
 
 
 function App() {
   return (
     <Router>
-      <Switch>
-        <Route path="/info/:id" component={Info} />
-      </Switch>
+      <Routes>
+        <Route path="/:id" element={<Info />} exact />
+      </Routes>
     </Router>
+
 
   );
 }
+
 
 export default App;
